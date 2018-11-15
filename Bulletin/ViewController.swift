53389d089be858dd02e6a7e5e588d39fb0f15bfc
Bret Alvey <bretalvey@macintosh-3.local>
2018-11-15 12:37:13 -0700

    Initial Commit

---
+++ Bulletin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bulletin
+//
+//  Created by Bret Alvey on 11/15/18.
+//  Copyright © 2018 Ethan Alvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
