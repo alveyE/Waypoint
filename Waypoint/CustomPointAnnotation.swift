f1daabcc30b237e3c90df0779356fdb0970ba0a5
Bret Alvey <bretalvey@macintosh-3.local>
2018-12-07 01:15:42 -0700

    loading screen

---
+++ Waypoint/CustomPointAnnotation.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPointAnnotation.swift
+//  Waypoint
+//
+//  Created by Bret Alvey on 12/7/18.
+//  Copyright © 2018 Ethan Alvey. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CustomPointAnnotation: MKAnnotationView {
+
+    var pinCustomImageName:String!
+   
+
+}
